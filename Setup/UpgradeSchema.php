43cd6e86ec90dc66b2f52bda1fb3f4b983b761ce
ConnectPOS <51113258+devconnectpos@users.noreply.github.com>
2021-10-08 11:09:44 +0700

    Release 21.10.08 (#34)
    Co-authored-by: Khue Quang Nguyen <khuenq@connectpos.com>

--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -13,10 +13,10 @@ use Magento\Eav\Model\Entity\Attribute\Set as AttributeSet;
 use Magento\Eav\Model\Entity\Attribute\SetFactory as AttributeSetFactory;
 use Magento\Framework\App\Area;
 use Magento\Framework\App\State;
-use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
 use Magento\Framework\Setup\UpgradeSchemaInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * @codeCoverageIgnore
@@ -62,18 +62,18 @@ class UpgradeSchema implements UpgradeSchemaInterface
         try {
             $this->state->emulateAreaCode(
                 Area::AREA_ADMINHTML, function (SchemaSetupInterface $setup, ModuleContextInterface $context) {
-                $installer = $setup;
-                $installer->startSetup();
-
                 if (version_compare($context->getVersion(), '0.0.2', '<')) {
-                    $this->addPhoneAttribute();
+                    $this->addPhoneAttribute($setup);
                 }
                 if (version_compare($context->getVersion(), '0.0.4', '<')) {
-                    $this->addAvatarAttribute();
-                    $this->addVerifaceAttribute();
+                    $this->addAvatarAttribute($setup);
+                    $this->addVerifaceAttribute($setup);
                 }
                 if (version_compare($context->getVersion(), '0.0.5', '<')) {
-                    $this->addGuestId();
+                    $this->addGuestId($setup);
+                }
+                if (version_compare($context->getVersion(), '0.0.6', '<')) {
+                    $this->add2ndTelephone($setup);
                 }
             }, [$setup, $context]
             );
@@ -83,20 +83,53 @@ class UpgradeSchema implements UpgradeSchemaInterface
             $logger->addWriter($writer);
             $logger->info('====> Failed to upgrade customer schema');
             $logger->info($e->getMessage()."\n".$e->getTraceAsString());
-        }
-
-        if (version_compare($context->getVersion(), '0.0.6', '<')) {
-            $this->add2ndTelephone();
-        }
-        if (version_compare($context->getVersion(), '0.0.7', '<')) {
-            $this->update2ndTelephone();
+            echo "Failed to upgrade customer schema: ".$e->getMessage()."\n";
         }
     }
 
-    protected function addPhoneAttribute()
+    /**
+     * Execute all schema upgrade at once
+     *
+     * @param SchemaSetupInterface $setup
+     * @param OutputInterface      $output
+     *
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @throws \Zend_Validate_Exception
+     */
+    public function execute(SchemaSetupInterface $setup, OutputInterface $output)
     {
+        $output->writeln('  |__ Add telephone attribute');
+        $this->addPhoneAttribute($setup);
+        $output->writeln('  |__ Add 2nd telephone attribute');
+        $this->add2ndTelephone($setup);
+        $output->writeln('  |__ Add avatar attribute');
+        $this->addAvatarAttribute($setup);
+        $output->writeln('  |__ Add veriface attribute');
+        $this->addVerifaceAttribute($setup);
+        $output->writeln('  |__ Add guest ID attribute');
+        $this->addGuestId($setup);
+    }
+
+    /**
+     * @param SchemaSetupInterface $setup
+     *
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @throws \Zend_Validate_Exception
+     */
+    protected function addPhoneAttribute(SchemaSetupInterface $setup)
+    {
+        $setup->startSetup();
         /** @var CustomerSetup $customerSetup */
         $customerSetup = $this->customerSetupFactory->create();
+
+        // Skip if the attribute exists
+        $attribute = $customerSetup->getEavConfig()->getAttribute(Customer::ENTITY, 'retail_telephone');
+
+        if ($attribute) {
+            $setup->endSetup();
+
+            return;
+        }
 
         $customerEntity = $customerSetup->getEavConfig()->getEntityType('customer');
         $attributeSetId = $customerEntity->getDefaultAttributeSetId();
@@ -109,34 +142,44 @@ class UpgradeSchema implements UpgradeSchemaInterface
             Customer::ENTITY,
             'retail_telephone',
             [
-                'type'         => 'varchar',
-                'label'        => 'Telephone',
-                'input'        => 'text',
-                'required'     => false,
-                'visible'      => true,
-                'user_defined' => true,
-                'sort_order'   => 1000,
-                'position'     => 1000,
-                'system'       => 0,
+                'type'               => 'varchar',
+                'label'              => 'Telephone',
+                'input'              => 'text',
+                'required'           => false,
+                'visible'            => true,
+                'user_defined'       => true,
+                'sort_order'         => 1000,
+                'position'           => 1000,
+                'system'             => 0,
+                'attribute_set_id'   => $attributeSetId,
+                'attribute_group_id' => $attributeGroupId,
+                'used_in_forms'      => ['adminhtml_customer'],
             ]
         );
 
-        $attribute = $customerSetup->getEavConfig()->getAttribute(Customer::ENTITY, 'retail_telephone')
-            ->addData(
-                [
-                    'attribute_set_id'   => $attributeSetId,
-                    'attribute_group_id' => $attributeGroupId,
-                    'used_in_forms'      => ['adminhtml_customer'],
-                ]
-            );
-
-        $attribute->save();
+        $setup->endSetup();
     }
 
-    protected function addAvatarAttribute()
+    /**
+     * @param SchemaSetupInterface $setup
+     *
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @throws \Zend_Validate_Exception
+     */
+    protected function addAvatarAttribute(SchemaSetupInterface $setup)
     {
+        $setup->startSetup();
         /** @var CustomerSetup $customerSetup */
         $customerSetup = $this->customerSetupFactory->create();
+
+        // Skip if the attribute exists
+        $attribute = $customerSetup->getEavConfig()->getAttribute(Customer::ENTITY, 'retail_avatar');
+
+        if ($attribute) {
+            $setup->endSetup();
+
+            return;
+        }
 
         $customerEntity = $customerSetup->getEavConfig()->getEntityType('customer');
         $attributeSetId = $customerEntity->getDefaultAttributeSetId();
@@ -149,34 +192,44 @@ class UpgradeSchema implements UpgradeSchemaInterface
             Customer::ENTITY,
             'retail_avatar',
             [
-                'type'         => 'varchar',
-                'label'        => 'Avatar',
-                'input'        => 'text',
-                'required'     => false,
-                'visible'      => true,
-                'user_defined' => true,
-                'sort_order'   => 1000,
-                'position'     => 1000,
-                'system'       => 0,
+                'type'               => 'varchar',
+                'label'              => 'Avatar',
+                'input'              => 'text',
+                'required'           => false,
+                'visible'            => true,
+                'user_defined'       => true,
+                'sort_order'         => 1000,
+                'position'           => 1000,
+                'system'             => 0,
+                'attribute_set_id'   => $attributeSetId,
+                'attribute_group_id' => $attributeGroupId,
+                'used_in_forms'      => ['adminhtml_customer'],
             ]
         );
 
-        $attribute = $customerSetup->getEavConfig()->getAttribute(Customer::ENTITY, 'retail_avatar')
-            ->addData(
-                [
-                    'attribute_set_id'   => $attributeSetId,
-                    'attribute_group_id' => $attributeGroupId,
-                    'used_in_forms'      => ['adminhtml_customer'],
-                ]
-            );
-
-        $attribute->save();
+        $setup->endSetup();
     }
 
-    protected function addVerifaceAttribute()
+    /**
+     * @param SchemaSetupInterface $setup
+     *
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @throws \Zend_Validate_Exception
+     */
+    protected function addVerifaceAttribute(SchemaSetupInterface $setup)
     {
+        $setup->startSetup();
         /** @var CustomerSetup $customerSetup */
         $customerSetup = $this->customerSetupFactory->create();
+
+        // Skip if the attribute exists
+        $attribute = $customerSetup->getEavConfig()->getAttribute(Customer::ENTITY, 'retail_veriface');
+
+        if ($attribute) {
+            $setup->endSetup();
+
+            return;
+        }
 
         $customerEntity = $customerSetup->getEavConfig()->getEntityType('customer');
         $attributeSetId = $customerEntity->getDefaultAttributeSetId();
@@ -189,34 +242,44 @@ class UpgradeSchema implements UpgradeSchemaInterface
             Customer::ENTITY,
             'retail_veriface',
             [
-                'type'         => 'varchar',
-                'label'        => 'Veriface',
-                'input'        => 'text',
-                'required'     => false,
-                'visible'      => true,
-                'user_defined' => true,
-                'sort_order'   => 1000,
-                'position'     => 1000,
-                'system'       => 0,
+                'type'               => 'varchar',
+                'label'              => 'Veriface',
+                'input'              => 'text',
+                'required'           => false,
+                'visible'            => true,
+                'user_defined'       => true,
+                'sort_order'         => 1000,
+                'position'           => 1000,
+                'system'             => 0,
+                'attribute_set_id'   => $attributeSetId,
+                'attribute_group_id' => $attributeGroupId,
+                'used_in_forms'      => ['adminhtml_customer'],
             ]
         );
 
-        $attribute = $customerSetup->getEavConfig()->getAttribute(Customer::ENTITY, 'retail_veriface')
-            ->addData(
-                [
-                    'attribute_set_id'   => $attributeSetId,
-                    'attribute_group_id' => $attributeGroupId,
-                    'used_in_forms'      => ['adminhtml_customer'],
-                ]
-            );
-
-        $attribute->save();
+        $setup->endSetup();
     }
 
-    protected function addGuestId()
+    /**
+     * @param SchemaSetupInterface $setup
+     *
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @throws \Zend_Validate_Exception
+     */
+    protected function addGuestId(SchemaSetupInterface $setup)
     {
+        $setup->startSetup();
         /** @var CustomerSetup $customerSetup */
         $customerSetup = $this->customerSetupFactory->create();
+
+        // Skip if the attribute exists
+        $attribute = $customerSetup->getEavConfig()->getAttribute(Customer::ENTITY, 'retail_guest_id');
+
+        if ($attribute) {
+            $setup->endSetup();
+
+            return;
+        }
 
         $customerEntity = $customerSetup->getEavConfig()->getEntityType('customer');
         $attributeSetId = $customerEntity->getDefaultAttributeSetId();
@@ -229,113 +292,75 @@ class UpgradeSchema implements UpgradeSchemaInterface
             Customer::ENTITY,
             'retail_guest_id',
             [
-                'type'         => 'varchar',
-                'label'        => 'Retail Guest Id',
-                'input'        => 'text',
-                'required'     => false,
-                'visible'      => true,
-                'user_defined' => true,
-                'sort_order'   => 1000,
-                'position'     => 1000,
-                'system'       => 0,
+                'type'               => 'varchar',
+                'label'              => 'Retail Guest Id',
+                'input'              => 'text',
+                'required'           => false,
+                'visible'            => true,
+                'user_defined'       => true,
+                'sort_order'         => 1000,
+                'position'           => 1000,
+                'system'             => 0,
+                'attribute_set_id'   => $attributeSetId,
+                'attribute_group_id' => $attributeGroupId,
+                'used_in_forms'      => ['adminhtml_customer'],
             ]
         );
 
-        $attribute = $customerSetup->getEavConfig()->getAttribute(Customer::ENTITY, 'retail_guest_id')
-                                   ->addData(
-                                       [
-                                           'attribute_set_id'   => $attributeSetId,
-                                           'attribute_group_id' => $attributeGroupId,
-                                           'used_in_forms'      => ['adminhtml_customer'],
-                                       ]
-                                   );
-
-        $attribute->save();
+        $setup->endSetup();
     }
 
-    protected function add2ndTelephone()
+    /**
+     * @param SchemaSetupInterface $setup
+     *
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @throws \Zend_Validate_Exception
+     */
+    protected function add2ndTelephone(SchemaSetupInterface $setup)
     {
+        $setup->startSetup();
         /** @var CustomerSetup $customerSetup */
         $customerSetup = $this->customerSetupFactory->create();
+
+        // Skip if the attribute exists
+        $attribute = $customerSetup->getEavConfig()->getAttribute(Customer::ENTITY, 'retail_telephone_2');
+
+        if ($attribute) {
+            $setup->endSetup();
+
+            return;
+        }
 
         $customerEntity = $customerSetup->getEavConfig()->getEntityType('customer');
         $attributeSetId = $customerEntity->getDefaultAttributeSetId();
 
         /** @var AttributeSet $attributeSet */
-        $attributeSet     = $this->attributeSetFactory->create();
+        $attributeSet = $this->attributeSetFactory->create();
         $attributeGroupId = $attributeSet->getDefaultGroupId($attributeSetId);
 
         $customerSetup->addAttribute(
             Customer::ENTITY,
             'retail_telephone_2',
             [
-                'type'         => 'varchar',
-                'label'        => 'Secondary Telephone',
-                'input'        => 'text',
-                'required'     => false,
-                'visible'      => true,
-                'user_defined' => true,
-                'sort_order'   => 1000,
-                'position'     => 1000,
-                'system'       => 0,
-                'is_used_in_grid' => true,
-                'is_visible_in_grid' => true
-            ]
-        );
-
-        $attribute = $customerSetup->getEavConfig()->getAttribute(Customer::ENTITY, 'retail_telephone_2')
-            ->addData(
-                [
-                    'attribute_set_id'   => $attributeSetId,
-                    'attribute_group_id' => $attributeGroupId,
-                    'used_in_forms'      => ['adminhtml_customer'],
-                ]
-            );
-
-        $attribute->save();
-    }
-
-    protected function update2ndTelephone()
-    {
-        /** @var CustomerSetup $customerSetup */
-        $customerSetup = $this->customerSetupFactory->create();
-
-        $customerEntity = $customerSetup->getEavConfig()->getEntityType('customer');
-        $attributeSetId = $customerEntity->getDefaultAttributeSetId();
-
-        /** @var AttributeSet $attributeSet */
-        $attributeSet     = $this->attributeSetFactory->create();
-        $attributeGroupId = $attributeSet->getDefaultGroupId($attributeSetId);
-
-        $customerSetup->addAttribute(
-            Customer::ENTITY,
-            'retail_telephone_2',
-            [
-                'type'         => 'varchar',
-                'label'        => 'Secondary Telephone',
-                'input'        => 'text',
-                'required'     => false,
-                'visible'      => true,
-                'user_defined' => true,
-                'sort_order'   => 1000,
-                'position'     => 1000,
-                'system'       => 0,
-                'is_used_in_grid' => true,
-                'is_visible_in_grid' => true,
+                'type'                  => 'varchar',
+                'label'                 => 'Secondary Telephone',
+                'input'                 => 'text',
+                'required'              => false,
+                'visible'               => true,
+                'user_defined'          => true,
+                'sort_order'            => 1000,
+                'position'              => 1000,
+                'system'                => 0,
+                'is_used_in_grid'       => true,
+                'is_visible_in_grid'    => true,
                 'is_filterable_in_grid' => true,
-                'is_searchable_in_grid' => true
+                'is_searchable_in_grid' => true,
+                'attribute_set_id'      => $attributeSetId,
+                'attribute_group_id'    => $attributeGroupId,
+                'used_in_forms'         => ['adminhtml_customer'],
             ]
         );
 
-        $attribute = $customerSetup->getEavConfig()->getAttribute(Customer::ENTITY, 'retail_telephone_2')
-            ->addData(
-                [
-                    'attribute_set_id'   => $attributeSetId,
-                    'attribute_group_id' => $attributeGroupId,
-                    'used_in_forms'      => ['adminhtml_customer'],
-                ]
-            );
-
-        $attribute->save();
+        $setup->endSetup();
     }
 }
